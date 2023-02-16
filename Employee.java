542aa2b6dc08541019f193eff8e743875327c445
kaushikbalajims-toast <kaushik.balaji.m.s@toasttab.com>
2023-02-16 08:24:05 +0530

    set methods updated

--- Employee.java
+++ Employee.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Employee{
     private static int counter = 1001;
     private int empID;
@@ -5,6 +7,10 @@ class Employee{
     private String designation;
     private String department;
     private double salary;
+
+    Employee(){
+        this.empID = counter++;
+    }
 
     Employee(String name, String dept, String desg, double sal){
         this.name = name;
@@ -34,13 +40,67 @@ class Employee{
         this.empID = id;
     }
     public void setName(String name){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            if(name.matches("^[A-Z][a-zA-Z ]+")){
+                break;
+            }
+            else{
+                System.out.println("Enter valid name (Start with caps)\n");
+                System.out.print("Enter name: ");
+                name = sc.nextLine();
+            }
+        }
         this.name = name;
     }
     public void setDesg(String desg){
-        this.designation = desg;
+        String[] designation_array = {"Manager", "Senior developer", "Software developer", "Intern"};
+        Scanner scin = new Scanner(System.in);
+        boolean invalidInput = false;
+        int desg_choice = 0;
+        do{
+            do{
+                invalidInput = false;
+                try{
+                    desg_choice = Integer.parseInt(desg);
+                }
+                catch(NumberFormatException e){
+                    System.out.println("Enter only one integer option\n");
+                    invalidInput = true;
+                    System.out.print("Designation choice\n1.Manager\n2.Senior developer\n3.Software developer\n4.Intern\nEnter designation choice: ");
+                    desg = scin.nextLine();
+                }
+            }while(invalidInput);
+            if(desg_choice>4 || desg_choice<0){
+                System.out.println("Enter an available option (1-4)\n");
+            }
+        }while(desg_choice>4 || desg_choice<0);
+
+        this.designation = designation_array[desg_choice-1];
     }
     public void setDept(String dept){
-        this.department = dept;
+        String[] department_array = {"R & D", "IT", "Admin", "HR", "Support"};
+        Scanner scin = new Scanner(System.in);
+        boolean invalidInput;
+        int dept_choice = 0;
+        do{
+            do{
+                invalidInput = false;
+                    try{
+                        dept_choice = Integer.parseInt(dept);
+                    }
+                    catch(NumberFormatException e){
+                        System.out.println("Enter only one integer option\n");
+                        invalidInput = true;
+                        System.out.print("\nDepartment choices \n1.R & D\n2.IT\n3.Admin\n4.HR\n5.Support\nEnter department choice:");
+                        dept = scin.nextLine();
+                    }
+            }while(invalidInput);
+            if(dept_choice>5 || dept_choice<0){
+                System.out.println("Enter an available option (1-5)\n");
+            }
+        }while(dept_choice>5 || dept_choice<0);
+        this.department = department_array[dept_choice-1];
     }
     public void setSal(double sal){
         this.salary = sal;
--- TestEmployee.java
+++ TestEmployee.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class TestEmployee{
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<Employee>();
-        String[] designation_array = {"Manager", "Senior developer", "Software developer", "Intern"};
-        String[] department_array = {"R & D", "IT", "Admin", "HR", "Support"};
-        String name = "";
-        int dept_choice = 0, desg_choice = 0, choice = 1;
+        int choice = 1;
         double salary = 0;
         Scanner scin = new Scanner(System.in);
         boolean invalidInput = false;
         while(choice == 1){
+            Employee employee = new Employee();
             do{
                 invalidInput = false;
                 try{
@@ -22,51 +20,13 @@ public class TestEmployee{
                     invalidInput = true;
                 }
             }while(invalidInput);
-            if(choice != 1){
-                break;
-            }
-                while(true){
-                    System.out.print("Enter name: ");
-                    name = scin.nextLine();
-                    if(name.matches("^[A-Z][a-zA-Z ]+")){
-                        break;
-                    }
-                    else{
-                        System.out.println("Enter valid name (Start with caps)\n");
-                    }
-                }
-                do{
-                    do{
-                        invalidInput = false;
-                            try{
-                                System.out.print("\nDepartment choices \n1.R & D\n2.IT\n3.Admin\n4.HR\n5.Support\nEnter department choice:");
-                                    dept_choice = Integer.parseInt(scin.nextLine());
-                            }
-                            catch(NumberFormatException e){
-                                System.out.println("Enter only one integer option\n");
-                                invalidInput = true;
-                            }
-                    }while(invalidInput);
-                    if(dept_choice>5 || dept_choice<0){
-                        System.out.println("Enter an available option (1-5)\n");
-                    }
-                }while(dept_choice>5 || dept_choice<0);
-                do{
-                    do{
-                        invalidInput = false;
-                        try{
-                            System.out.print("Designation choice\n1.Manager\n2.Senior developer\n3.Software developer\n4.Intern\nEnter designation choice: ");
-                            desg_choice = Integer.parseInt(scin.nextLine());
-                        }
-                        catch(NumberFormatException e){
-                            System.out.println("Enter only one integer option\n");
-                            invalidInput = true;
-                        }
-                    }while(invalidInput);
-                    if(desg_choice>4 || desg_choice<0){
-                        System.out.println("Enter an available option (1-4)\n");
-                    }
-                }while(desg_choice>4 || desg_choice<0);
+            if(choice == 1){
+                System.out.print("Enter name: ");
+                employee.setName(scin.nextLine());
+                System.out.print("\nDepartment choices \n1.R & D\n2.IT\n3.Admin\n4.HR\n5.Support\nEnter department choice:");
+                employee.setDept(scin.nextLine());
+                System.out.print("Designation choice\n1.Manager\n2.Senior developer\n3.Software developer\n4.Intern\nEnter designation choice: ");
+                employee.setDesg(scin.nextLine());
                 do{
                     invalidInput = false;
                     try{
@@ -77,11 +37,18 @@ public class TestEmployee{
                         System.out.println("Enter valid salary");
                         invalidInput = true;
                     }
+                    if(salary < 10000){
+                        System.out.println("Enter 5 figure salary");
+                        invalidInput = true;
+                    }
                 }while(invalidInput);
-                employees.add(new Employee(name, department_array[dept_choice-1] , designation_array[desg_choice-1], salary));
+                employee.setSal(salary);
+                
+                employees.add(employee);
                 System.out.println();
+                }
             }
-        scin.close();
+            scin.close();
         if(employees.size() != 0){
             System.out.println("..... All Employees available .....");
             for (int i = 0; i < employees.size(); i++) {
